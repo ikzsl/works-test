1a68cbd651f40f90b5d29b698a9fa8572ff1f1d4
Муслим Джалалудинов <muslim.dzhalaludinov@osinit.com>
2021-04-22 05:50:47 +0300

    add axios

--- src/App.js
+++ src/App.js
@@ -1,55 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import axios from 'axios';
+import { routes } from './routes';
 
 function App() {
+
+  const [userLogin, setUserLogin] = useState('');
+  const [userAvatar, setUserAvatar] = useState('');
+  
+  useEffect(() => {
+    axios
+    .get(routes.searchUsersUrl(), {
+      params: {
+        q: 'ikzsl',
+      },
+    })
+    .then((response) => {
+      console.log(response.data.items[0]);
+      setUserAvatar(response.data.items[0].avatar_url);
+      setUserLogin(response.data.items[0].login)
+    });
+  }, [])
+
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+    <div className='App'>
+      <header className='App-header'>
+        {/* <img src={logo} className='App-logo' alt='logo' /> */}
+        {/* <Counter /> */}
+        <div>
+          <p>{userLogin}</p>
+          <img src={userAvatar} alt=''/>
+        </div>
       </header>
     </div>
   );
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.github.com';
+
+export const routes = {
+  searchUsersUrl: () => '/search/users',
+  // userLoginUrl: () => 'users/login',
+  // getProfileUrl: () => 'user',
+  // getArticleUrl: (slug) => `articles/${slug}`,
+  // getArticlesListUrl: (limit = 10, offset = 0) => `articles?limit=${limit}&offset=${offset}`,
+  // setFavoriteArticleURL: (slug) => `articles/${slug}/favorite`,
+  // articlePostUrl: () => 'articles',
+  // articleEditUrl: (slug) => `articles/${slug}`,
+  // articleDeleteUrl: (slug) => `articles/${slug}`,
+};
